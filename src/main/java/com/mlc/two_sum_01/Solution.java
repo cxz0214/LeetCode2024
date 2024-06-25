96087f630661228f57f3fd14fdc73ca44fd9ca91
mlc <cxz1721529255@gmail.com>
2024-06-25 17:30:56 +0800

    两数字和

---
+++ src/main/java/com/mlc/two_sum_01/Solution.java
@@ -0,0 +1,18 @@
+package com.mlc.two_sum_01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	public int [] twoSum(int[] nums,int target){
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i = 0; i < nums.length;i++){
+			int r = target - nums[i];
+			if(map.containsKey(r)){
+				return new int[] {map.get(r),i};
+			}
+			map.put(nums[i],i);
+		}
+		return new int[]{-1,-1};
+	}
+}
