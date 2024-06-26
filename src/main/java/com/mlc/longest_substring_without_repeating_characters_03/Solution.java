b3ef85de9d1ce905a5ed4793997313adaad2f06b
mlc <cxz1721529255@gmail.com>
2024-06-26 10:41:29 +0800

    无重复字符的最长子串

---
+++ src/main/java/com/mlc/longest_substring_without_repeating_characters_03/Solution.java
@@ -0,0 +1,18 @@
+package com.mlc.longest_substring_without_repeating_characters_03;
+
+public class Solution {
+	public int lengthOfLongestSubstring(String s) {
+		if(s.isEmpty()) return 0;
+		Map<Character,Integer> charIndexMap = new HashMap<>();
+		int ans = 0;
+		for(int end = 0, start = 0; end < s.length(); end++){
+			char curChar = s.charAt(end);
+			if(charIndexMap.containsKey(curChar)){
+				start = Math.max(start,charIndexMap.get(curChar) + 1);
+			}
+			charIndexMap.put(curChar,end);
+			ans = Math.max(ans,end - start + 1);
+		}
+		return ans;
+	}
+}
