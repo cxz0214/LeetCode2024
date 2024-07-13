377d34a168e0c8a43c36ca3b3c671629101143c9
MorningLightCode <1721529255@qq.com>
2024-07-13 14:13:49 +0800

    跳跃游戏II

---
+++ src/main/java/com/mlc/jump_game_II_45/Solution.java
@@ -0,0 +1,20 @@
+package com.mlc.jump_game_II_45;
+
+public class Solution {
+	
+	// 这个大概理解了，但是还需要在熟悉熟悉
+	public int jump(int[] nums) {
+		int end = 0;
+		int maxPosition = 0;
+		int steps = 0;
+		
+		for(int i = 0; i < nums.length - 1; i++){
+			maxPosition = Math.max(maxPosition,nums[i] + i);
+			if(i == end){
+				end = maxPosition;
+				steps ++;
+			}
+		}
+		return steps;
+	}
+}
